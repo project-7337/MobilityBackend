d973ee25d4e68c32966f33b59d35307ccafd492e
Aditya Kumar <adityak007mr@gmail.com>
2020-09-13 21:42:56 +0530

    lambdas with api gateway upa and running

--- funcCountry/main.go
+++ funcCountry/main.go
@@ -1,19 +1,41 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"funcCountry/handler"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
+
+// Response ->
+type Response events.APIGatewayProxyResponse
 
 func main() {
 	lambda.Start(CountryHandler)
 }
 
 //CountryHandler -> lambda handler
-func CountryHandler(ctx context.Context) ([]string, error) {
+func CountryHandler(ctx context.Context) (Response, error) {
 
 	finalList := handler.APIHandler()
-	return finalList, nil
+	var buf bytes.Buffer
+	body, err := json.Marshal(finalList)
+	if err != nil {
+		return Response{StatusCode: 404}, err
+	}
+	json.HTMLEscape(&buf, body)
+	resp := Response{
+		StatusCode:      200,
+		IsBase64Encoded: false,
+		Body:            buf.String(),
+		Headers: map[string]string{
+			"Content-Type":                "application/json",
+			"X-MyCompany-Func-Reply":      "hello-handler",
+			"Access-Control-Allow-Origin": "*",
+		},
+	}
+	return resp, nil
 }
--- lambda_backend/main.go
+++ lambda_backend/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"backend/apihandler"
 	"bytes"
-	"context"
 	"encoding/json"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -18,8 +18,11 @@ func main() {
 }
 
 //BackendHandler -> lambda handler
-func BackendHandler(ctx context.Context, request events.APIGatewayProxyRequest) (Response, error) {
-	param1 := request.QueryStringParameters["country"]
+func BackendHandler(request events.APIGatewayProxyRequest) (Response, error) {
+	param1, found := request.QueryStringParameters["country"]
+	if !found {
+		fmt.Println("No QP Available")
+	}
 	finalList := apihandler.APIHandler(param1)
 	var buf bytes.Buffer
 	body, err := json.Marshal(finalList)
@@ -32,8 +35,9 @@ func BackendHandler(ctx context.Context, request events.APIGatewayProxyRequest) 
 		IsBase64Encoded: false,
 		Body:            buf.String(),
 		Headers: map[string]string{
-			"Content-Type":           "application/json",
-			"X-MyCompany-Func-Reply": "hello-handler",
+			"Content-Type":                "application/json",
+			"X-MyCompany-Func-Reply":      "hello-handler",
+			"Access-Control-Allow-Origin": "*",
 		},
 	}
 	return resp, nil
